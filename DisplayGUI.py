6939fc2ec6afd73ee93651f6aa1e687ba090d995
Xerminator13 <52582665+Xerminator13@users.noreply.github.com>
2023-04-15 17:33:53 -0600

    Video renders

--- DisplayGUI.py
+++ DisplayGUI.py
@@ -1,53 +1,36 @@
+"""
+1 - https://stackoverflow.com/questions/48364168/flickering-video-in-opencv-tkinter-integration
+"""
+
 import tkinter as tk
 import cv2
+from RealtimeVideo import get_video_frames
+import threading
+from PIL import Image, ImageTk
 
 
-def show_color1():
-    print("red")
+def run_video(video_label):
+    width = 960
+    height = 540
 
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Failed to get from camera, exiting")
+        exit(1)
 
-def show_color2():
-    print("orange")
+    while True:
+        frame, colorFrame = get_video_frames(cap, width, height)
+        img = Image.fromarray(colorFrame)
+        imgtk = ImageTk.PhotoImage(image=img)
+        video_label.config(image=imgtk)
+        video_label.image = imgtk  # 1 - This one line stops flickering
 
-
-def show_color3():
-    print("yellow")
-
-
-def show_color4():
-    print("green")
-
-
-def show_color5():
-    print("blue")
-
-
-def show_color6():
-    print("purple")
-
-
-def show_color7():
-    print("black")
-
-
-def show_color8():
-    print("brown")
-
-
-def show_color9():
-    print("grey")
-
-
-def show_color10():
-    print("white")
-
-
-def print_color(color_val):
-    print(color_val)
+    cap.release()
+    cv2.destoryAllWindows()
 
 
 def renderHeader(UI):
-    headerFrame = tk.Frame(master=UI, width=1600, height=100, bg="red")
+    headerFrame = tk.Label(master=UI, width=1600, height=100, bg="red")
     titleLabel = tk.Label(headerFrame, text="Camera Color Conversion")
     titleLabel.pack()
 
@@ -60,10 +43,16 @@ def renderColorPallete(UI):
     return palleteFrame
 
 
+def print_color(color_val):
+    print(color_val)
+
+
 def renderButtons(color_pallete_frame, button_array):
+    width = 20
+    height = width // 2
     for button in button_array:
         temp_frame = tk.Frame(master=color_pallete_frame)
-        temp_button = tk.Button(temp_frame, text=button, command=lambda:print_color(button))
+        temp_button = tk.Button(temp_frame, height=height, width=width, bg=button, text=button, command=lambda color=button:print_color(color))
         temp_button.pack(side=tk.LEFT)
 
         temp_frame.pack(side=tk.LEFT)
@@ -72,54 +61,25 @@ def renderButtons(color_pallete_frame, button_array):
 def main():
     UI = tk.Tk()
 
-    customCameraTitleFrame = renderHeader(UI)
+    # customCameraTitleFrame = renderHeader(UI)
 
     colorPalletteFrame = renderColorPallete(UI)
 
-    # colors_array = ["red", "orange", "yellow", "green", "blue", "purple", "black", "brown", "grey", "white"]
-    # renderButtons(colorPalletteFrame, colors_array)
-
-    color1 = tk.Button(colorPalletteFrame, text="red", command=show_color1)
-    color1.pack(side=tk.LEFT)
-
-    color2 = tk.Button(colorPalletteFrame, text="orange", command=show_color2)
-    color2.pack(side=tk.LEFT)
-
-    color3 = tk.Button(colorPalletteFrame, text="yellow", command=show_color3)
-    color3.pack(side=tk.LEFT)
-
-    color4 = tk.Button(colorPalletteFrame, text="green", command=show_color4)
-    color4.pack(side=tk.LEFT)
-
-    color5 = tk.Button(colorPalletteFrame, text="blue", command=show_color5)
-    color5.pack(side=tk.LEFT)
-
-    color6 = tk.Button(colorPalletteFrame, text="purple", command=show_color6)
-    color6.pack(side=tk.LEFT)
-
-    color7 = tk.Button(colorPalletteFrame, text="black", command=show_color7)
-    color7.pack(side=tk.LEFT)
-
-    color8 = tk.Button(colorPalletteFrame, text="brown", command=show_color8)
-    color8.pack(side=tk.LEFT)
-
-    color9 = tk.Button(colorPalletteFrame, text="grey", command=show_color9)
-    color9.pack(side=tk.LEFT)
-
-    color10 = tk.Button(colorPalletteFrame, text="white", command=show_color10)
-    color10.pack(side=tk.LEFT)
+    colors_array = ["red", "orange", "yellow", "green", "blue", "purple", "black", "brown", "grey", "white"]
+    renderButtons(colorPalletteFrame, colors_array)
 
     beforeColorChangeFrame = tk.Frame(master=UI, width=800, height=600, bg="blue")
     beforeColorChangeLabel = tk.Label(beforeColorChangeFrame, text="Video Before Color Changes")
-    # BE SURE TO RUN THE VIDEO FRAME ON While True, or only one frame will be captured
+    video_label = tk.Label(master=beforeColorChangeFrame)
+    video_label.pack()
     beforeColorChangeLabel.pack()
 
     afterColorChangeFrame = tk.Frame(master=UI, width=800, height=600, bg="green")
     afterColorChangeLabel = tk.Label(afterColorChangeFrame, text="Video After Color Changes")
     afterColorChangeLabel.pack()
 
-    customCameraTitleFrame.pack(fill=tk.BOTH, side=tk.TOP)
-    customCameraTitleFrame.pack_propagate(False)
+    # customCameraTitleFrame.pack(fill=tk.BOTH, side=tk.TOP)
+    # customCameraTitleFrame.pack_propagate(False)
 
     colorPalletteFrame.pack(fill=tk.BOTH, side=tk.BOTTOM)
     colorPalletteFrame.pack_propagate(False)
@@ -130,7 +90,14 @@ def main():
     afterColorChangeFrame.pack(fill=tk.BOTH, side=tk.RIGHT, expand=True)
     afterColorChangeFrame.pack_propagate(False)
 
+    video_input_thread = threading.Thread(target=run_video, args=(video_label,))
+
+    video_input_thread.start()
+
+    # UI.update()
     UI.mainloop()
+
+    video_input_thread.join()
 
 
 if __name__ == "__main__":

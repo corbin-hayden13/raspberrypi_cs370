d936b6fc04d9a1d94ce9037e2cb2646d7fc440b3
jmichaelc4 <122645642+jmichaelc4@users.noreply.github.com>
2023-04-24 17:07:30 -0600

    Update ColorUIElement.py

--- ColorUIElement.py
+++ ColorUIElement.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from ColorEditor import rgb_to_name
 
-
 class ColorUIElement:
     def update_button(self):
         self.color_name = rgb_to_name(self.rgb)
@@ -33,18 +32,16 @@ class ColorUIElement:
         self.g_scale = tk.Scale(self.frame, from_=0, to=255, orient='horizontal', command=lambda new_val:self.set_g(new_val))
         self.b_scale = tk.Scale(self.frame, from_=0, to=255, orient='horizontal', command=lambda new_val:self.set_b(new_val))
 
+        self.color_label = tk.Label(self.frame, width=20, height=2, bg=self.color_name, text=self.color_name)
+        self.color_button = tk.Button(self.frame, height=2, width=20, bg=self.color_name, text=self.color_name)
+
+        self.color_label.pack()
         self.r_scale.pack()
         self.g_scale.pack()
         self.b_scale.pack()
-
-        self.color_label = tk.Label(self.frame, width=20, height=2, bg=self.color_name, text=self.color_name)
-        self.color_button = tk.Button(self.frame, height=height, width=width, bg=self.color_name, text=self.color_name)
-
-        self.color_label.pack()
         self.color_button.pack()
 
         self.frame.pack(side=tk.LEFT)
 
     def set_button_command(self, new_command):
         self.color_button.configure(command=lambda a=self.org_rgb, b=self.rgb:new_command(a, b))
-

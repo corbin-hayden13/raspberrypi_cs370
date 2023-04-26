Fixing masking

ColorEditor.py
@@ -7,9 +7,8 @@ import numpy as np
 import pandas as pd
 
 
-curr_new_rgb = [0, 0, 0]
+curr_new_bgr = [0, 0, 0]
 new_frame_mask = []
-
 artificial_bound = 0
 
 
@@ -41,29 +40,44 @@ for a in range(len(names)):
     color_dict[str(rgb_list[a])] = names[a]
 
 
-def make_color_mask(color_frame, new_rgb):
-    new_row = [new_rgb for pixel in range(len(color_frame[0]))]
+def make_color_mask(color_frame, new_bgr):
+    new_row = [new_bgr for pixel in range(len(color_frame[0]))]
     ret_mask = [new_row for row in range(len(color_frame))]
 
     return ret_mask
 
 
-def change_color(color_frame, find_rgb, new_rgb, changed_queue):
-    global curr_new_rgb, new_frame_mask, artificial_bound
-    if curr_new_rgb != new_rgb or new_frame_mask == []:
-        curr_new_rgb = new_rgb
-        new_frame_mask = make_color_mask(color_frame, new_rgb)
-        new_frame_mask = np.array(new_frame_mask, dtype="uint8")
-
+def calc_lower_upper(find_bgr):
     lower = []
     upper = []
 
-    for a in range(len(find_rgb)):
-        sub_val = find_rgb[a] - artificial_bound if find_rgb[a] - artificial_bound > 0 else 0
-        add_val = find_rgb[a] + artificial_bound if find_rgb[a] + artificial_bound < 255 else 255
+    for b in range(len(find_bgr)):  # Edit b, g, and r values
+        sub_val = find_bgr[b] - artificial_bound if find_bgr[b] - artificial_bound > 0 else 0
+        add_val = find_bgr[b] + artificial_bound if find_bgr[b] + artificial_bound < 255 else 255
 
         lower.append(sub_val)
         upper.append(add_val)
+
+    return lower, upper
+
+
+def bgr_equals(new_bgr):
+    global curr_new_bgr
+    for c in range(len(new_bgr)):
+        if new_bgr[c] != curr_new_bgr[c]:
+            return False
+
+    return True
+
+
+def change_color(color_frame, find_bgr, new_bgr, changed_queue):
+    global curr_new_bgr, new_frame_mask, artificial_bound
+    if not bgr_equals(new_bgr) or new_frame_mask == []:
+        curr_new_bgr = new_bgr
+        new_frame_mask = make_color_mask(color_frame, new_bgr)
+        new_frame_mask = np.array(new_frame_mask, dtype="uint8")
+
+    lower, upper = calc_lower_upper(find_bgr)
 
     lower = np.array(lower, dtype="uint8")
     upper = np.array(upper, dtype="uint8")
@@ -84,11 +98,11 @@ def rgb_to_name(rgb_val):
     except KeyError:
         min_diff = 255 * 3 + 1
         found_ind = 0
-        for a in range(len(rgb_list)):
-            temp_diff = abs(sum(rgb_list[a]) - sum(rgb_val))
+        for d in range(len(rgb_list)):
+            temp_diff = abs(sum(rgb_list[d]) - sum(rgb_val))
             if temp_diff < min_diff:
                 min_diff = temp_diff
-                found_ind = a
+                found_ind = d
 
         color_name = color_dict[str(rgb_list[found_ind])]
         return color_name
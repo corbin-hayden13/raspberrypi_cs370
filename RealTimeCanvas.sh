2f205eacba0d887e525e541fbcb8013f61f33fc2
jmichaelc4 <122645642+jmichaelc4@users.noreply.github.com>
2023-05-03 09:25:41 -0600

    Update RealTimeCanvas.sh

--- RealTimeCanvas.sh
+++ RealTimeCanvas.sh
@@ -2,4 +2,8 @@
 # RealTimeCanvas.sh
 # Run from directory containing project files
 
+# Joseph Michael Coffey IV – jmichaelc4@gmail.com – jmcoffey@colostate.edu 
+# Hayden Corbin - haydenfcorbin@gmail.com - hayden.corbin@colostate.edu 
+# Reilly Bergeron - reillybergeron@gmail.com - reillyjb@colostate.edu 
+
 python3 DisplayGUI.py
